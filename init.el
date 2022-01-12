cd730e9c5efbe334b9d99ecc99f0d9d442b03933
leanhtai01 <leanhtai01@gmail.com>
2022-01-12 12:05:46 +0700

    display fill column indicator in all programming modes

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
 ;; load customizations
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file)
+
+;; display fill column indicator in all programming modes
+(add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
