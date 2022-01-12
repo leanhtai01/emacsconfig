3ac21a39f6b3db529a7b0a58717ed046639bc37d
leanhtai01 <leanhtai01@gmail.com>
2022-01-12 19:09:34 +0700

    make background color of selected region more readable

--- custom.el
+++ custom.el
@@ -12,4 +12,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 113 :width normal))))
+ '(region ((t (:extend t :background "#add6ff" :distant-foreground "gtk_selection_fg_color")))))
