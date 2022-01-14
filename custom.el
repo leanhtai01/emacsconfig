9bcd2236db8453040df1910f89d7feb5375b7f14
leanhtai01 <leanhtai01@gmail.com>
2022-01-14 23:36:32 +0700

    set magit-diff-refine-hunk to all

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-diff-refine-hunk 'all)
  '(package-archives
    '(("melpa" . "https://melpa.org/packages/")
      ("gnu" . "https://elpa.gnu.org/packages/")))
