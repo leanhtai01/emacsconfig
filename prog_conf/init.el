cda7b7e9feba68d47239cbb2603a39a56ed142fb
leanhtai01 <leanhtai01@gmail.com>
2022-01-15 11:25:48 +0700

    add configuration for C++

--- prog_conf/init.el
+++ prog_conf/init.el
@@ -1,2 +1,7 @@
 ;; display fill column indicator in all programming modes
 (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
+
+;; load configurations for corresponding language
+(let ((current-dir (concat user-emacs-directory "prog_conf/")))
+  ;; C++
+  (load (concat current-dir "cpp.el")))
