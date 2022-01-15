44f0ed77d5048630257824dec37676e0ddff7e6d
leanhtai01 <leanhtai01@gmail.com>
2022-01-15 18:14:56 +0700

    use lsp with sgml-mode, css-mode and js-mode

--- prog_conf/web.el
+++ prog_conf/web.el
@@ -1,3 +1,8 @@
 ;; use emmet-mode with sgml-mode and css-mode
 (add-hook 'sgml-mode-hook 'emmet-mode)
 (add-hook 'css-mode-hook  'emmet-mode)
+
+;; use lsp with sgml-mode, css-mode and js-mode
+(add-hook 'sgml-mode-hook #'lsp)
+(add-hook 'css-mode-hook #'lsp)
+(add-hook 'js-mode-hook #'lsp)
