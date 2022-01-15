8258c06b6201ccb03ce064f3047ea430399af9b4
leanhtai01 <leanhtai01@gmail.com>
2022-01-15 14:17:10 +0700

    configure lsp-mode's performance

--- prog_conf/init.el
+++ prog_conf/init.el
@@ -4,6 +4,10 @@
 ;; use company mode
 (add-hook 'prog-mode-hook 'company-mode)
 
+;; configure lsp-mode's performance
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024))
+
 ;; load configurations for corresponding language
 (let ((current-dir (concat user-emacs-directory "prog_conf/")))
   ;; C
