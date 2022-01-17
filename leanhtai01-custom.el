ca1b971a10367d6f2761e06ca77c93372771c7a7
leanhtai01 <leanhtai01@gmail.com>
2022-01-17 23:04:52 +0700

    treat all themes as safe

--- leanhtai01-custom.el
+++ leanhtai01-custom.el
@@ -10,6 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes t)
  '(magit-diff-refine-hunk 'all)
  '(package-archives
    '(("melpa" . "https://melpa.org/packages/")
