33f3574a677a2fbb2ffcb8bfb9a60b1ccc28c1a2
leanhtai01 <leanhtai01@gmail.com>
2022-01-17 23:22:27 +0700

    highlighting region using default color from theme

--- leanhtai01-custom.el
+++ leanhtai01-custom.el
@@ -27,8 +27,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 113 :width normal))))
- '(region ((t (:extend t :background "#add6ff" :distant-foreground "gtk_selection_fg_color")))))
+ '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 113 :width normal)))))
 
 (provide 'leanhtai01-custom)
 
