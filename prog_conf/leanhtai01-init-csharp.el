a9d98a1d7e1f03996c149404c395134a2f0d4bd3
leanhtai01 <leanhtai01@gmail.com>
2022-01-22 20:20:56 +0700

    add file leanhtai01-init-csharp.el

---
+++ prog_conf/leanhtai01-init-csharp.el
@@ -0,0 +1,12 @@
+;;; leanhtai01-init-csharp.el --- configuration for csharp
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
+(add-hook 'csharp-mode-hook #'lsp)
+
+(provide 'leanhtai01-init-csharp)
+
+;;; leanhtai01-init-csharp.el ends here
