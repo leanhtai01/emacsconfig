9cb6e8e1f8cb8ad541f55d73352a81fb889410da
leanhtai01 <leanhtai01@gmail.com>
2022-01-24 12:40:59 +0700

    make diff refine added more dark

--- leanhtai01-custom.el
+++ leanhtai01-custom.el
@@ -32,7 +32,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 113 :width normal))))
- '(diff-refine-added ((t (:background "#22aa22" :foreground "#f4f4f4"))))
+ '(diff-refine-added ((t (:background "#228622" :foreground "#f4f4f4"))))
  '(diff-refine-removed ((t (:background "#aa2222" :foreground "#f4f4f4"))))
  '(magit-diff-removed-highlight ((t (:extend t :background "#663333" :foreground "#f4f4f4")))))
 
