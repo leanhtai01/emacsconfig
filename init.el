ba8fd9c7438370b5435a384a81f22ade2cfb0a47
leanhtai01 <leanhtai01@gmail.com>
2023-12-26 18:15:21 +0700

    replace ivy with helm

--- init.el
+++ init.el
@@ -29,35 +29,20 @@
 ;; use which-key
 (which-key-mode)
 
-;; configure ivy-mode
-(ivy-mode)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
-;; enable this if you want `swiper' to use it
-;; (setq search-default-mode #'char-fold-to-regexp)
-(global-set-key "\C-s" 'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>") 'ivy-resume)
-(global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-(global-set-key (kbd "C-c g") 'counsel-git)
-(global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
-(global-set-key (kbd "C-x l") 'counsel-locate)
-(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+;; configure helm
+(helm-mode)
+(define-key global-map [remap find-file] #'helm-find-files)
+(define-key global-map [remap execute-extended-command] #'helm-M-x)
+(define-key global-map [remap switch-to-buffer] #'helm-mini)
 
 ;; use C-x C-u to convert entire region to uppercase
 (put 'upcase-region 'disabled nil)
 
 ;; use C-x C-l to convert entire region to lowercase
 (put 'downcase-region 'disabled nil)
+
+;; change lsp key prefix
+(setq lsp-keymap-prefix "C-c l")
 
 ;; load programming configurations
 (require 'leanhtai01-prog-init)
